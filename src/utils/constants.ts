22118e53d9c24b8fd487f5d24c221fbc97cea56f
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-03-01 10:10:07 +0100

    constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+    BrightBlack: "\x1b[90m",
+    NC: "\x1b[0m",
+    Cyan: "\x1b[36m",
+    Red: "\x1b[31m",
+    Green: "\x1b[32m",
+    BrightBlue: "\x1b[94m",
+    Yellow: "\x1b[33m"
+};
