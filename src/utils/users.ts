bbf7d789cb39fc282a6ab3068f4a07fbfe431c62
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-03-01 13:35:14 +0100

    constructClient function

---
+++ src/utils/users.ts
@@ -0,0 +1,13 @@
+// Modules
+import { Socket } from "socket.io";
+import { User } from "gizmo-api";
+
+// Types
+import { Client } from "../types";
+
+export function constructClient (socket: Socket, user: User): Client {
+    return {
+        socket,
+        user
+    };
+}
