ee5c107d4f7809852848a22dce879d621a270690
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-03-01 19:01:43 +0100

    construct room

--- src/utils/rooms.ts
+++ src/utils/rooms.ts
@@ -1,3 +1,13 @@
+// Types
+import { Room } from "../types";
+
 export function sanitizeRoomId (roomId: string) {
     return JSON.stringify(roomId); // DIY string sanitization, please don't actually do this
+}
+
+export function createRoom (roomId: string): Room {
+    return {
+        id: roomId,
+        data: null
+    };
 }
