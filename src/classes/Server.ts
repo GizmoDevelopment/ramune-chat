9a0c9d5f69e85a1827c108136b798e911ad4247e
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-03-01 19:59:12 +0100

    fully implement rooms and syncing

--- src/classes/Server.ts
+++ src/classes/Server.ts
@@ -8,10 +8,10 @@ import io, { Socket } from "socket.io";
 // Utils
 import logger from "../utils/logger";
 import { constructClient, constructExtendedUser } from "../utils/users";
-import { sanitizeRoomId } from "../utils/rooms";
+import { constructRoom, sanitizeRoomId, updateRoom } from "../utils/rooms";
 
 // Types
-import { Client } from "../types";
+import { Client, Room } from "../types";
 
 export default class Server {
 
@@ -19,6 +19,7 @@ export default class Server {
     private readonly ioServer: io.Server;
 
     clients: Map<string, Client> = new Map();
+    rooms: Map<string, Room> = new Map();
 
     constructor (port: number) {
 
@@ -59,6 +60,183 @@ export default class Server {
 
     private getClientFromSocketId (socketId: string) {
         return this.clients.get(socketId);
+    }
+
+    private roomExists (roomId: string) {
+        return this.rooms.has(roomId);
+    }
+
+    private updateRoom (socket: Socket, type: string, roomId: string, data?: Record<string, any>) {
+        if (this.roomExists(roomId)) {
+
+            const room = this.rooms.get(roomId);
+
+            if (room) {
+                switch (type) {
+                    case "change_host":
+    
+                        if (data && typeof data.host === "string") {
+
+                            const newHostClient = this.getClientFromSocketId(data.host);
+
+                            if (newHostClient) {
+
+                                const updatedRoom = updateRoom(room, { host: data.host });
+
+                                this.modifyClientData(socket, {
+                                    hostOfRoom: null
+                                });
+        
+                                this.modifyClientData(newHostClient, {
+                                    hostOfRoom: roomId
+                                });
+
+                                this.rooms.set(roomId, updatedRoom);
+                                socket.to(roomId).emit("client:update_room", updatedRoom);
+                            }
+                        }
+
+                        break;
+                    case "update_data":
+
+                        if (typeof data === "object") {
+                            
+                            const updatedRoom = updateRoom(room, { data });
+
+                            this.rooms.set(roomId, updatedRoom);
+                            socket.to(roomId).emit("client:update_room", updatedRoom);
+                        }
+
+                        break;
+                    default:
+                }
+            }
+        }
+    }
+
+    private createRoom (socket: Socket, roomId: string) {
+        if (!this.roomExists(roomId)) {
+
+            // New room (promote to host)
+            this.modifyClientData(socket, {
+                hostOfRoom: roomId
+            });
+
+            this.rooms.set(roomId, constructRoom(socket, roomId));
+        }
+    }
+
+    private async removeRoom (roomId: string) {
+        if (this.roomExists(roomId)) {
+            
+            const socketsInRoom = await this.ioServer.to(roomId).allSockets();
+
+            if (socketsInRoom) {
+                socketsInRoom.forEach(socketId => {
+                    
+                    const client = this.getClientFromSocketId(socketId);
+
+                    if (client) {
+                        this.leaveRoom(client.socket, roomId);
+                    }
+
+                });
+            }
+
+            this.rooms.delete(roomId);
+        }
+    }
+
+    private async joinRoom (socket: Socket, roomId: string, callback?: Function) {
+
+        const
+            sanitizedRoomId = sanitizeRoomId(roomId),
+            user = this.getClientFromSocket(socket)?.user;
+
+        if (user) {
+
+            this.leaveAllSocketRooms(socket);
+
+            if ((await this.ioServer.to(sanitizedRoomId).allSockets()).size > 0) {
+
+                // Existing room
+                this.modifyClientData(socket, {
+                    hostOfRoom: null
+                });
+
+            } else {
+
+                this.createRoom(socket, sanitizedRoomId);
+                logger.info(`{${ socket.id }} Client created roomID {${ sanitizedRoomId }}`);
+
+            }
+
+            socket.join(sanitizedRoomId);
+            this.ioServer.to(sanitizedRoomId).emit("client:join_room", user);
+
+            const
+                socketsInRoom = await this.ioServer.to(sanitizedRoomId).allSockets(),
+                listOfUsersInRoom: Record<string, User> = {};
+
+            socketsInRoom.forEach(socketId => {
+                if (socketId !== socket.id) {
+                    
+                    const user = this.getClientFromSocketId(socketId)?.user;
+
+                    if (user) {
+                        listOfUsersInRoom[user.id] = user;
+                    }
+                }
+            });
+
+            if (callback) {
+                callback({
+                    type: "success",
+                    message: listOfUsersInRoom
+                });
+            }
+
+            logger.info(`{${ socket.id }} Client joined roomID {${ sanitizedRoomId }}`);
+            
+        } else if (callback) {
+            callback({
+                type: "error",
+                message: "Client doesn't exist"
+            });
+        }
+    }
+
+    private async leaveRoom (socket: Socket, roomId: string, callback?: Function) {
+
+        const
+            sanitizedRoomId = sanitizeRoomId(roomId),
+            user = this.getClientFromSocket(socket)?.user;
+
+        if (user) {
+
+            socket.leave(sanitizedRoomId);
+
+            if ((await this.ioServer.to(sanitizedRoomId).allSockets()).size > 0) {
+                this.ioServer.to(sanitizedRoomId).emit("client:leave_room", user.id);
+            } else {
+                this.removeRoom(sanitizedRoomId);
+            }
+
+            if (callback) {
+                callback({
+                    type: "success",
+                    message: "Successfully left room"
+                });
+            }
+
+            logger.info(`{${ socket.id }} Client left roomID {${ sanitizedRoomId }}`);
+
+        } else if (callback) {
+            callback({
+                type: "error",
+                message: "Client doesn't exist"
+            });
+        }
     }
 
     private leaveAllSocketRooms (socket: Socket) {
@@ -148,60 +326,8 @@ export default class Server {
         });
 
         socket.on("client:join_room", async (data: { roomId: string }, callback: Function) => {
-            if (this.socketExists(socket) && data?.roomId && data?.roomId?.startsWith("room:")) {
-
-                const
-                    sanitizedRoomId = sanitizeRoomId(data.roomId),
-                    user = this.getClientFromSocket(socket)?.user;
-
-                if (user) {
-
-                    this.leaveAllSocketRooms(socket);
-
-                    if ((await this.ioServer.to(sanitizedRoomId).allSockets()).size > 0) {
-                        // Existing room
-                        this.modifyClientData(socket, {
-                            hostOfRoom: null
-                        });
-                    } else {
-                        // New room (promote to host)
-                        this.modifyClientData(socket, {
-                            hostOfRoom: sanitizedRoomId
-                        });
-                    }
-
-                    socket.join(sanitizedRoomId);
-                    this.ioServer.to(sanitizedRoomId).emit("client:join_room", user);
-
-                    const
-                        socketsInRoom = await this.ioServer.to(sanitizedRoomId).allSockets(),
-                        listOfUsersInRoom: Record<string, User> = {};
-
-                    socketsInRoom.forEach(socketId => {
-                        if (socketId !== socket.id) {
-                            
-                            const user = this.getClientFromSocketId(socketId)?.user;
-
-                            if (user) {
-                                listOfUsersInRoom[user.id] = user;
-                            }
-                        }
-                    });
-
-                    callback({
-                        type: "success",
-                        message: listOfUsersInRoom
-                    });
-
-                    logger.info(`{${ socket.id }} Client joined roomID {${ sanitizedRoomId }}`);
-                    
-                } else {
-                    callback({
-                        type: "error",
-                        message: "Client doesn't exist"
-                    });
-                }
-
+            if (data?.roomId) {
+                this.joinRoom(socket, data.roomId, callback);
             } else {
                 callback({
                     type: "error",
@@ -211,31 +337,8 @@ export default class Server {
         });
 
         socket.on("client:leave_room", (data: { roomId: string }, callback: Function) => {
-            if (this.socketExists(socket) && data?.roomId && data?.roomId?.startsWith("room:")) {
-
-                const
-                    sanitizedRoomId = sanitizeRoomId(data.roomId),
-                    user = this.getClientFromSocket(socket)?.user;
-
-                if (user) {
-
-                    socket.leave(sanitizedRoomId);
-                    this.ioServer.to(sanitizedRoomId).emit("client:leave_room", user.id);
-
-                    callback({
-                        type: "success",
-                        message: "Successfully left room"
-                    });
-
-                    logger.info(`{${ socket.id }} Client left roomID {${ sanitizedRoomId }}`);
-
-                } else {
-                    callback({
-                        type: "error",
-                        message: "Client doesn't exist"
-                    });
-                }
-
+            if (data?.roomId) {
+                this.leaveRoom(socket, data.roomId, callback);
             } else {
                 callback({
                     type: "error",
@@ -245,50 +348,75 @@ export default class Server {
         });
 
         socket.on("client:send_message", (data: { content: string }) => {
-            if (this.socketExists(socket)) {
 
-                const client = this.getClientFromSocket(socket);
+            const client = this.getClientFromSocket(socket);
 
-                if (client) {
+            if (client) {
 
-                    const user = client.user;
+                const user = client.user;
 
-                    this.ioServer.sockets.emit("client:send_message", {
-                        id: Math.floor(Math.random() * 10000000),
-                        type: "text",
-                        content: data.content,
-                        author: {
-                            id: user.id,
-                            username: user.uid,
-                            avatar: user.avatar
-                        }
+                this.ioServer.sockets.emit("client:send_message", {
+                    id: Math.floor(Math.random() * 10000000),
+                    type: "text",
+                    content: data.content,
+                    author: {
+                        id: user.id,
+                        username: user.uid,
+                        avatar: user.avatar
+                    }
+                });
+            }
+        });
+
+        socket.on("client:sync_player", (data: { timestamp: number, paused: boolean }, callback: Function) => {
+            
+            const client = this.getClientFromSocket(socket);
+
+            if (client) {
+
+                const {
+                    hostOfRoom
+                } = client.data;
+
+                if (hostOfRoom) {
+                    socket.to(hostOfRoom).emit("client:sync_player", {
+                        timestamp: Number(data.timestamp) || 0,
+                        paused: !!data.paused || false
+                    });
+                } else {
+                    callback({
+                        type: "error",
+                        message: "You aren't the host"
                     });
                 }
             }
         });
 
-        socket.on("client:sync_player", (data: { timestamp: number, paused: boolean }, callback: Function) => {
-            if (this.socketExists(socket)) {
+        socket.on("client:update_room", (data: { showId: string, episodeId: string }, callback: Function) => {
 
-                const client = this.getClientFromSocket(socket);
+            const client = this.getClientFromSocket(socket);
 
-                if (client) {
+            if (client) {
 
-                    const {
-                        hostOfRoom
-                    } = client.data;
+                const {
+                    hostOfRoom
+                } = client.data;
 
-                    if (hostOfRoom) {
-                        socket.to(hostOfRoom).emit("client:sync_player", {
-                            timestamp: Number(data.timestamp) || 0,
-                            paused: !!data.paused || false
-                        });
-                    } else {
-                        callback({
-                            type: "error",
-                            message: "You aren't the host"
-                        });
-                    }
+                if (hostOfRoom) {
+
+                    const newRoomContent = {
+                        showId: data.showId,
+                        episodeId: data.episodeId
+                    };
+
+                    this.updateRoom(socket, "update_data", hostOfRoom, newRoomContent);
+                    socket.to(hostOfRoom).emit("client:update_room", newRoomContent);
+
+                } else {
+                    callback({
+                        type: "error",
+                        message: "You aren't the host"
+                    });
                 }
             }
         });
