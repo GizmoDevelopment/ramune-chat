545eb521d11a99b8ecf66036cc90973d854df970
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-03-01 10:09:43 +0100

    copy custom logger from gizmo-bot

---
+++ src/utils/logger.ts
@@ -0,0 +1,61 @@
+// Variables
+import { COLORS } from "./constants";
+
+/**
+ * Returns formatted date label for use in console
+ */
+function getDateLabel () {
+    return `${ COLORS.BrightBlack }[${
+        new Date().toLocaleString("default", {
+            month: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+            hour12: false
+        })
+    }]${ COLORS.NC }`;
+}
+
+/**
+ * Logs a message to the console
+ * @param message 
+ * @param color 
+ */
+function log (message: string, color: string = COLORS.NC) {
+
+    // Color words in 'single quotes'
+    message = message.replace(/('.*?')/g, `${COLORS.BrightBlue}$1${color}`);
+
+    console.log(`${getDateLabel()}${color} ${message}${COLORS.NC}`);
+}
+
+/**
+ * Logs a basic message to the console
+ * @param message 
+ */
+function info (message: string) {
+    log(message);
+}
+
+/**
+ * Logs a success message to the console
+ * @param message
+ */
+function success (message: string) {
+    log(message, COLORS.Green);
+}
+
+/**
+ * Logs an error message to the console
+ * @param message
+ */
+function error (message: any) {
+    log(message?.stack ? message.stack : message, COLORS.Red);
+}
+
+export default {
+    info,
+    success,
+    error
+};
