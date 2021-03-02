24c3c4f3b33205e16ce146d6398f139aa0f3e4c5
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-03-02 12:18:31 +0100

    prepareRoomForSending with overloads

--- src/utils/rooms.ts
+++ src/utils/rooms.ts
@@ -1,8 +1,11 @@
 // Modules
 import { Socket } from "socket.io";
 
+// Classes
+import Server from "../classes/Server";
+
 // Types
-import { Room } from "../types";
+import { Room, SimpleRoom } from "../types";
 
 export function sanitizeRoomId (roomId: string) {
     return roomId; // DIY string sanitization, please don't actually do this
@@ -25,4 +28,34 @@ export function updateRoom (oldRoom: Room, newRoom: Record<string, any>): Room {
             ...(newRoom?.data || {})
         }
     };
+}
+
+export function prepareRoomForSending (server: Server, room: Room): SimpleRoom | undefined;
+export function prepareRoomForSending (server: Server, room: string): SimpleRoom | undefined;
+export function prepareRoomForSending (server: Server, room: Room | string): SimpleRoom | undefined {
+
+    let _room: Room | undefined;
+
+    // RoomID was passed
+    if (typeof room === "string") {
+        const _roomResult = server.rooms.get(room);
+        if (_roomResult) _room = _roomResult;
+    } else {
+        _room = room;
+    }
+
+    if (_room) {
+
+        const hostUser = server.getUserFromSocketId(_room.host);
+
+        if (hostUser) {
+            return {
+                id: _room.id,
+                host: hostUser,
+                users: [],
+                data: _room.data
+            };
+        }
+    }
+
 }
