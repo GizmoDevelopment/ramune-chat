3f069420587ae07c0f9964a6f2b6fe5d151fd5a4
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-03-03 12:21:34 +0100

    no need to warn about no port env variable

--- src/index.ts
+++ src/index.ts
@@ -30,5 +30,4 @@ if (process.env.NODE_ENV === "production") {
     process.on("unhandledRejection", logger.error);
 }
 
-if (!process.env.PORT) throw Error("Missing environmental variable 'PORT'");
 new Server(Number(process.env.PORT) || 1337);
