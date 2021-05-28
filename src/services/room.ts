1bee40ffc825e5c69c0fea2b8d35f88fe6ee633d
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-28 19:14:34 +0200

    joinRoom & leaveRoom room service methods

--- src/services/room.ts
+++ src/services/room.ts
@@ -4,10 +4,12 @@ import Service from "@classes/Service";
 
 // Types
 import { Room } from "@typings/room";
+import { User } from "gizmo-api/lib/types";
 
 class RoomService extends Service {
 
 	rooms: Map<string, Room> = new Map();
+	userToRoomMap: Record<string, string> = {};
 
 	constructor (cluster: Cluster) {
 		super("room", cluster);
@@ -17,20 +19,36 @@ class RoomService extends Service {
 		this.rooms.set(room.id, room);
 	}
 
-	removeRoom (room: Room) {
-		this.rooms.delete(room.id);
-	}
-
-	roomExists (roomId: string): boolean {
-		return this.rooms.has(roomId);
-	}
-
 	getRoom (roomId: string): Room | null {
 		return this.rooms.get(roomId) || null;
 	}
 
 	getRooms (): Room[] {
 		return Array.from(this.rooms.values());
+	}
+
+	getUserCurrentRoom (user: User): Room | null {
+		if (this.userToRoomMap[user.id]) {
+			
+			const room = this.getRoom(this.userToRoomMap[user.id]);
+
+			if (room) {
+				return room;
+			} else {
+				return null;
+			}
+
+		} else {
+			return null;
+		}
+	}
+
+	joinRoom (user: User, room: Room) {
+		room.join(user);
+	}
+
+	leaveRoom (user: User, room: Room) {
+		room.leave(user);
 	}
 
 }
