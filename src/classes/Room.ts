33b37824146a0f40627bd2ec51c2e4194dc9fb2d
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-28 19:11:55 +0200

    Room join and leave methods

--- src/classes/Room.ts
+++ src/classes/Room.ts
@@ -1,6 +1,9 @@
 // Modules
 import { randomUUID } from "crypto";
 
+// Utils
+import logger from "@utils/logger";
+
 // Types
 import { RoomData } from "@typings/room";
 import { User } from "gizmo-api/lib/types";
@@ -29,6 +32,21 @@ export default class Room implements RoomConstruct {
 		this.host = host;
 		this.users.push(host);
 
+	}
+
+	join (user: User) {
+
+		this.users = this.users.filter(_user => _user.id !== user.id);
+		this.users.push(user);
+
+		logger.info(`[R-${ this.id }] [${ user.username }] Joined room`);
+	}
+
+	leave (user: User) {
+
+		this.users = this.users.filter(_user => _user.id !== user.id);
+
+		logger.info(`[R-${ this.id }] [${ user.username }] Left room`);
 	}
 
 
