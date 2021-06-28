7c517713f595bd9b6ad0240177cbdb3f180cb98a
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-06-28 15:12:41 +0200

    getEpisode & getEpisodeById

--- src/utils/ramune.ts
+++ src/utils/ramune.ts
@@ -6,7 +6,10 @@ import { SHOW_ENDPOINT } from "@utils/constants";
 import logger from "@utils/logger";
 
 // Types
-import { Show } from "@typings/show";
+import { Show, Episode } from "@typings/show";
+
+// Variables
+const showCache: Map<string, Show> = new Map();
 
 export async function getShow (showId: string): Promise<Show|null> {
 	try {
@@ -23,4 +26,62 @@ export async function getShow (showId: string): Promise<Show|null> {
 		logger.error(err);
 		return null;
 	}
+}
+
+export async function getEpisode (showId: string, episodeId: number): Promise<Episode|null> {
+	try {
+
+		let episode: Episode | null = null;
+
+		if (showCache.has(showId)) {
+
+			const _show = showCache.get(showId);
+
+			if (_show) {
+
+				const _episode = getEpisodeById(_show, episodeId);
+
+				if (_episode) {
+					episode = _episode;
+				}
+
+			}
+
+		}
+
+		if (!episode) {
+
+			const _show = await getShow(showId);
+
+			if (_show) {
+
+				const _episode = getEpisodeById(_show, episodeId);
+
+				if (_episode) {
+					episode = _episode;
+				}
+			}
+
+		}
+
+		return episode;
+	} catch (err) {
+		logger.error(err);
+		return null;
+	}
+}
+
+export function getEpisodeById (show: Show, episodeId: number): Episode | null {
+
+	let _episode: Episode | null = null;
+
+	show.seasons.forEach(({ episodes }) => {
+		episodes.forEach((episode: Episode) => {
+			if (episode.id === episodeId) {
+				_episode = episode;
+			}
+		});
+	});
+
+	return _episode;
 }
