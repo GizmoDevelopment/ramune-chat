46f35e2f0ebb344c906a10ceb71c0b00fa423fb6
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-07-12 14:55:34 +0200

    give messages random UUIDs

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,3 +1,6 @@
+// Modules
+import { v4 as uuidv4 } from "uuid";
+
 // Utils
 import { sanitize } from "./essentials";
 
@@ -7,6 +10,7 @@ import { Message } from "@typings/message";
 
 export function constructMessage (user: User, messageContent: string): Message {
 	return {
+		id: uuidv4(),
 		user,
 		content: sanitize(messageContent.trim().slice(0, 500))
 	};
