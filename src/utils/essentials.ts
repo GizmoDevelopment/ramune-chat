9b790d47bc3ce1a5abcaf2e06296be94bb0092ef
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-08-02 15:47:17 +0200

    support protocol prop on response factory

--- src/utils/essentials.ts
+++ src/utils/essentials.ts
@@ -2,22 +2,27 @@
 import sanitizeHtml from "sanitize-html";
 
 // Types
-import { ErrorResponse, SuccessResponse } from "@typings/main";
+import { BaseResponse, ErrorResponse, SuccessResponse } from "@typings/main";
 
-export function createResponse<T> (type: "success", data: T): SuccessResponse<T>;
-export function createResponse (type: "error", message: string): ErrorResponse;
-export function createResponse (type: "success" | "error", dataOrMessage: any) {
-	if (type === "success") {
-		return {
-			type: "success",
-			data: dataOrMessage
-		};
-	} else {
-		return {
-			type: "error",
-			message: dataOrMessage
-		};
+export function createResponse<T> (type: "success", data: T, protocol?: string): SuccessResponse<T>;
+export function createResponse (type: "error", message: string, protocol?: string): ErrorResponse;
+export function createResponse (type: "success" | "error", dataOrMessage: any, protocol?: string) {
+
+	const res: BaseResponse = {
+		type
+	};
+
+	if (protocol) {
+		res.protocol = protocol;
 	}
+
+	if (type === "success") {
+		(res as SuccessResponse<any>).data = dataOrMessage;
+	} else {
+		(res as ErrorResponse).message = dataOrMessage;
+	}
+
+	return res;
 }
 
 export function sanitize (input: string, options?: sanitizeHtml.IOptions): string {
